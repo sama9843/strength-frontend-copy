bbb7422c7719e85508cc6636a549820766338a99
Timur Tripp <timur@timurtripp.com>
2024-04-03 12:10:30 -0600

    Adds Form component

---
+++ src/component/Form.jsx
@@ -0,0 +1,33 @@
+import { Component } from "react";
+
+function handleSubmit(event) {
+  event.preventDefault();
+  alert(`Your message: '${event.target.message.value}'`);
+}
+
+export default class Form extends Component {
+  render() {
+    return (
+      <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-message">
+              Your message
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500" id="inline-message" name="message" type="text" />
+          </div>
+        </div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button className="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
