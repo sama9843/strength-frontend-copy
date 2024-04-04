dee159aebfa8e2d10b86ee64e1b3f728bb8c1beb
Timur Tripp <timur@timurtripp.com>
2024-04-03 18:37:03 -0600

    Alters form to actually submit the request

---
+++ src/lib/endpoint.js
@@ -0,0 +1,21 @@
+export const ENDPOINT = process.env.NODE_ENV == 'production' ? 'https://ep-strength.silvercrater.dev/api' : 'http://localhost:8080/api';
+export const HTTP_GET = 'GET';
+export const HTTP_POST = 'POST';
+export const HTTP_PUT = 'PUT';
+export const HTTP_DELETE = 'DELETE';
+
+
+export async function request(target, method, body) {
+  const fetchParams = {
+    method: method
+  };
+  if (body) {
+    fetchParams.body = JSON.stringify(body);
+  }
+  const response = await fetch(`${ENDPOINT}/${target}`, fetchParams);
+  if (response.status === 200) {
+    return await response.json();
+  } else {
+    return null;
+  }
+}
