afbe8f582c704bbd4b1042032c3c316691e69687
Timur Tripp <timur@timurtripp.com>
2024-05-01 17:48:38 -0600

    Updates acceptance tests (3)

--- acceptance-test/test/test.js
+++ acceptance-test/test/test.js
@@ -17,16 +17,13 @@ after(async function () {
 
 describe('articles', function () {
   // User story: As a user, I want to view fitness headlines
-  // User story: As a user, I want to be able to follow links to the articles I am served
   it('should list articles', async function () {
     this.timeout(3000);
     const articles = await driver.findElements(By.css('.article-list li a'));
-    const hostname = new URL(url).hostname;
     assert(articles.length > 4);
     articles.forEach(async function(article) {
       assert(await article.isDisplayed());
       assert((await article.getText()).length > 0);
-      assert((new URL(await article.getAttribute('href')).hostname) != hostname);
     });
   });
 
