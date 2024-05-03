7b455e31156dbb72e5ee5680805f23dbdb063ef7
samuel-mast <84589616+samuel-mast@users.noreply.github.com>
2024-05-03 17:27:46 -0600

    Gym API Test 2

--- src/components/gyms/GymRatings.jsx
+++ src/components/gyms/GymRatings.jsx
@@ -3,14 +3,14 @@ import React, { useEffect, useState } from 'react';
 export default function GymRatings({ error }) {
   const [data, setData] = useState([]);
   const places = [];
-  const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json/?location=40.0150%2C-105.270546&radius=1000&keyword=gym&key=AIzaSyDwYKdRzM84YjBmH6QLpxFXfpRPEIbNn8k";
+  const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.0150%2C-105.270546&radius=1000&keyword=gym&key=AIzaSyDwYKdRzM84YjBmH6QLpxFXfpRPEIbNn8k";
    
   useEffect(() => {
     let ignore = false;
     if (!error) {
       (async function() {
         //const response = (await new Request(url).background(errorCallback)).response;
-        const response = await ((await fetch(url))).json();
+        const response = await ((await fetch(url,{mode:'cors',headers:{'Access-Control-Allow-Origin':'*'}}))).json();
         if (!ignore) {
           for (let googlePlace of response.results) {
             var place = {};
