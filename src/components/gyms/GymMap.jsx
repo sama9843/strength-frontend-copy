b450d36ed63837e24b7b52b0027eaa01ff258776
samuel-mast <84589616+samuel-mast@users.noreply.github.com>
2024-05-03 19:22:47 -0600

    Marker Test 2

--- src/components/gyms/GymMap.jsx
+++ src/components/gyms/GymMap.jsx
@@ -15,6 +15,15 @@ const center = {
 export default function GymMap() {
 
   const [marks, setMarks] = useState([]);
+  const gyms = [
+    {lat:40.0164221,lng:-105.279267},
+    {lat:40.01543360000001,lng:-105.2612796},
+    {lat:40.01421,lng:-105.2626733},
+    {lat:40.0226344,lng:-105.261516},
+    {lat:40.0195165,lng:-105.2599253},
+    {lat:40.01533269999999,lng:-105.2597696},
+    {lat:40.02096362989273,lng:-105.2664713201073}];
+
   const onMapClick = (e) => {
     setMarks((current) => [
         ...current,
@@ -25,14 +34,20 @@ export default function GymMap() {
       ]);
     };
   return (
-    <GoogleMap mapContainerStyle={mapContainerStyle} zoom={13} center={center}onClick={onMapClick}> 
-      <Marker position={{lat: 40.0150, lng: 254.7356}} />          
+    <GoogleMap mapContainerStyle={mapContainerStyle} zoom={13} center={center}onClick={onMapClick}>
+      {gyms.map((marker) => (
+          <Marker 
+            position={{ 
+              lat: marker.lat,
+              lng: marker.lng 
+            }} icon={"http://maps.google.com/mapfiles/ms/icons/purple-dot.png"} />
+      ))}         
       {marks.map((marker) => (
           <Marker 
             position={{ 
               lat: marker.lat,
               lng: marker.lng 
-            }} />
+            }}  />
       ))}
     </GoogleMap>
   );
